83e53058ee9462a49a0f36fa6bbd0aebff4f5393
Azure7896 <azurusek@gmail.com>
2023-04-11 23:01:07 +0200

    Add status colors 04.11

--- src/app/ticket.service.ts
+++ src/app/ticket.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Ticket} from "./ticket";
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +12,9 @@ export class TicketService {
 
   constructor(private http: HttpClient) {
     this.url='http://localhost:8080/';
+  }
+
+  getAllTickets(): Observable<Ticket[]> {
+    return this.http.get<Ticket[]>(this.url);
   }
 }
---
+++ src/app/ticket.ts
@@ -0,0 +1,9 @@
+export class Ticket {
+
+  ticketNumber: string;
+  name: string;
+  ticketStatus: string;
+  user: string;
+  createdDate: string;
+  timeToEnd: string;
+}
