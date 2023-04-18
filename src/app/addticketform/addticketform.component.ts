0776b7e2b902292c0ba275ea0efc7d531a2adfed
Azure7896 <azurusek@gmail.com>
2023-04-18 02:38:19 +0200

    Set page title 04.18

--- src/app/addticketform/addticketform.component.ts
+++ src/app/addticketform/addticketform.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {tick} from "@angular/core/testing";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import {config} from "rxjs";
 @Component({
   selector: 'app-addticketform',
   templateUrl: './addticketform.component.html',
@@ -9,13 +10,15 @@ import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 })
 export class AddticketformComponent {
 
-  public Editor = ClassicEditor;
+   public editor = ClassicEditor;
 
 ticketForm = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(25)]),
     body: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(25)]),
   })
 
+  constructor() {
+  }
   getErrorMessage(field): string {
     if (field.hasError('required')) {
       return 'Pole jest puste, podaj wartość';
