79311fcc39e53ef4d8a4860ace5ce77c497b4454
Azure7896 <azurusek@gmail.com>
2023-05-04 20:51:56 +0200

    Complete ticketForm form group, add saveTicket POST method to the ticket service 5.04

--- src/app/addticketform/addticketform.component.ts
+++ src/app/addticketform/addticketform.component.ts
@@ -3,6 +3,7 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {tick} from "@angular/core/testing";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import {config} from "rxjs";
+import {TicketService} from "../ticket.service";
 @Component({
   selector: 'app-addticketform',
   templateUrl: './addticketform.component.html',
@@ -12,12 +13,14 @@ export class AddticketformComponent {
 
    public editor = ClassicEditor;
 
+   responseStatus;
+
 ticketForm = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(25)]),
-    body: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(25)]),
+    ticketBody: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(25)]),
   })
 
-  constructor() {
+  constructor(private ticketService: TicketService) {
   }
   getErrorMessage(field): string {
     if (field.hasError('required')) {
@@ -25,6 +28,13 @@ ticketForm = new FormGroup({
     } else {
       return 'Niepoprawna ilość znaków'
     }
+  }
+
+  createNewTicket(): void {
+    this.ticketService.saveTicket(this.ticketForm).subscribe(response => {
+      this.responseStatus = response.status;
+      console.warn(this.responseStatus);
+    });
   }
 
   protected readonly tick = tick;
