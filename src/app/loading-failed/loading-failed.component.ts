d31230752e5988599d904eae5dfecc939e9def86
Azure7896 <azurusek@gmail.com>
2023-07-30 19:42:56 +0200

    Add error page 7.30

---
+++ src/app/loading-failed/loading-failed.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import {SharedService} from "../shared.service";
+
+@Component({
+  selector: 'app-loading-failed',
+  templateUrl: './loading-failed.component.html',
+  styleUrls: ['./loading-failed.component.css']
+})
+export class LoadingFailedComponent {
+
+  details = false;
+
+  constructor(public sharedService: SharedService) {
+
+  }
+
+  reloadCurrentPage() {
+    window.location.reload();
+  }
+}
